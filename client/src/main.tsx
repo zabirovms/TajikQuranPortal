972d8778300db9ed4a04748c2d448a82c0b52659
zabirovms <zabirovms@gmail.com>
2025-05-16 11:48:22 +0500

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,8 +3,8 @@ import App from "./App";
 import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
-import { Toaster } from "@/components/ui/toaster";
-import { ThemeProvider } from "@/hooks/useTheme";
+import { Toaster } from "./components/ui/toaster";
+import { ThemeProvider } from "./hooks/useTheme";
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
